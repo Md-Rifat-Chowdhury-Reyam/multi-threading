0b0a1426c5ab10f197c6ad229e185be68711ca88
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-28 22:17:40 +0600

    life cycle of thread

--- src/Thread/lifecycle/TicketBook.java
+++ src/Thread/lifecycle/TicketBook.java
@@ -12,7 +12,7 @@ public class TicketBook implements Runnable {
             e.printStackTrace();
         }
 
-        System.out.println("State of ticket booking thread while main thread is waiting : " );
+        System.out.println("State of ticket booking thread while main thread is waiting : " + TicketSystem.mainThread.getState());
 
         try {
             Thread.sleep(100);
@@ -24,5 +24,19 @@ public class TicketBook implements Runnable {
 
 class TicketSystem implements Runnable
 {
+   public static Thread mainThread;
 
+    @Override
+    public void run() {
+
+        TicketBook TcktBook = new TicketBook();
+        Thread MThd = new Thread(TcktBook);
+
+        System.out.println("State after creating new Ticket booking thread : " + MThd.getState());
+
+        MThd.start();
+
+        System.out.println("State after starting ticket booking thread : " + MThd.getState());
+
+    }
 }
